ef833530f89f5a03e8b45a5ba5b0bf97bab03852
susrith <susrith.maddi05@gmail.com>
2024-05-30 13:34:59 +0000

    .

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,27 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class LightDarkMode extends Component {
+  state = {dark: false}
+  back = () => {
+    this.setState(ps => ({dark: !ps.dark}))
+  }
+  backgorund = () => {
+    return this.state.dark ? 'dark-mode' : 'light-mode'
+  }
+  render() {
+    const hi = this.backgorund()
+    const buttontext = this.state.dark ? 'Light Mode' : 'Dark Mode'
+    return (
+      <div className={hi}>
+        <h1>Click To Change Mode</h1>
+        <button type="button" onClick={this.back}>
+          {buttontext}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default LightDarkMode
